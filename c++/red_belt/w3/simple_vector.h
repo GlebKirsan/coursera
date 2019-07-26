ec796f52acd8a62e2967fcb177c17127e90bd9d9
glkv <glkv@protonmail.ch>
2019-07-26 20:53:24 +0300

    SimpleVector

--- c++/red_belt/w3/simple_vector.h
+++ c++/red_belt/w3/simple_vector.h
@@ -1,24 +1,66 @@
 #pragma once
 
 #include <cstdlib>
+#include <iostream>
 
-// Реализуйте шаблон SimpleVector
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector();
-  explicit SimpleVector(size_t size);
-  ~SimpleVector();
+  SimpleVector(){
+      data = nullptr;
+      end_ = nullptr;
+      size_ = 0;
+      cap = 0;
+  }
 
-  T& operator[](size_t index);
+  explicit SimpleVector(size_t size){
+      data = new T[size];
+      end_ = data + size;
+      size_ = size;
+      cap = size_;
+  }
 
-  T* begin();
-  T* end();
+  ~SimpleVector() {
+      delete[] data;
+  }
 
-  size_t Size() const;
-  size_t Capacity() const;
-  void PushBack(const T& value);
+  T& operator[](size_t index){
+      return data[index];
+  }
+
+  T* begin(){ return data; }
+  T* end(){ return end_; }
+
+  size_t Size() const { return size_; }
+  size_t Capacity() const { return cap; }
+  void PushBack(const T& value){
+      if (size_ == cap){
+          Resize();
+      }
+     
+      data[size_] = value;
+      size_ += 1;
+      end_ = data + size_;
+  }
 
 private:
-  // Добавьте поля для хранения данных вектора
+  void Resize(){
+      cap = cap == 0 ? cap + 1 : 2*cap;
+      SimpleVector<T> tmp(size_);
+      for(size_t i = 0; i < size_; ++i){
+          tmp[i] = data[i];
+      }
+
+      delete[] data;
+      data = new T[cap];
+      for(size_t i = 0; i < size_; ++i){
+          data[i] = tmp[i];
+      }
+  }
+
+
+  T* data{nullptr};
+  T* end_{nullptr};
+  size_t size_;
+  size_t cap;
 };
